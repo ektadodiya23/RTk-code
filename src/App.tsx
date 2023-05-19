9a006da150858cf53a1733d133fa069667ed1483
Ekta Dodiya <Ekta.Dodiya@MSBC-MAINFRAME.LCL>
2023-05-19 12:43:07 +0100

    Create a React app and publish it to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/example/Navbar';
-
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
 
 
 function App() {
   return (
     <div>
-      <Navbar/>
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route path="*" element={<Navbar />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
